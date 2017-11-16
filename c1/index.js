ab9bc8fcd5c9ad6426fcf9f501e3cfc804562e27
pr0pellerhead <bojan.gavrovski@gmail.com>
2017-11-16 13:29:34 +0100

    Intro to Js

---
+++ c1/index.js
@@ -0,0 +1,21 @@
+console.log('Hello world!');
+
+
+
+var ime = "Bojan"; // string (низа од карактери)
+var godini = 35; // number
+var visina = 182.5; // number
+var temperatura = -22.5; // number
+var classEnded = true; // boolean
+
+console.log(Ime);
+
+godinaNaRagjanje = 1982; // camel case style
+godina_na_ragjanje = 1982; // snake case style
+godinanaragjanje = 1982; // BAD CASE!
+
+
+
+// "..."
+// '...'
+// `...`
